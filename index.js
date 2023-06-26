3d2229e6328ca2756929f39d5473ab077f40323e
Nenoeglah <134705870+Nenoeglah@users.noreply.github.com>
2023-06-26 13:45:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+  fetch('https://anapioficeandfire.com/api/books')
+  .then((res) => res.json())
+  .then((data) => renderBooks(data));
 
-function renderBooks(books) {
+ }
+
+ function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
+   books.forEach(book => {
+     const h2 = document.createElement('h2');
+     h2.innerHTML = book.name;
+     main.appendChild(h2);
+   });
+   }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+ 
+document.addEventListener('DOMContentLoaded',  function() {
+  fetchBooks()
+ });
+ function initialize(){
+  fetchBooks()
+ }
+ initialize()
